dba0a9135d2d7d6754898128d829e54ffd619982
Surya Jayanata <sjayanata00@gmail.com>
2025-09-21 00:43:47 +0700

    feat(ui): Tambah halaman berita + update landing page section rekomendasi jurusan & carousel departemen (belum fiks)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/informasi/berita', function () {
+    return view('informasi.berita');
+});
+Route::get('/informasi/berita/berita-lengkap1', function () {
+    return view('informasi.berita_lengkap1');
+});
